b509cec31685a78bf385dabd78c295c9c206b363
bluejoyq <bluejoy32@gmail.com>
2022-10-27 20:17:19 +0900

    bef make table

--- src/socket/socket.module.ts
+++ src/socket/socket.module.ts
@@ -1,11 +1,12 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
+import { DispenserModule } from 'src/dispenser/dispenser.module';
 import { DispenserSocketGateway } from './dispenser-socket.gateway';
 import { SocketService } from './socket.service';
 import { UserSocketGateway } from './user-socket.gateway';
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule), forwardRef(() => DispenserModule)],
   controllers: [],
   exports: [SocketService],
   providers: [SocketService, UserSocketGateway, DispenserSocketGateway],
